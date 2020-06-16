bfe2ec0f17336613209d81157aafbbc8ea21346c
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-06-16 16:17:31 -0400

    Add files via upload

---
+++ exome/step5/snake_submit_step5_hg19indel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=snake_indel19
+#SBATCH --mem=3000M
+#SBATCH -c 1
+#SBATCH -t 72:00:00
+
+source /cluster/home/amammoli/.bashrc
+module load python3
+
+output_dir="/cluster/projects/bhklab/Data/TFRI_Exome" snakemake -s /cluster/projects/bhklab/Data/TFRI_Exome/step5/Snakefile_vcftomaf_hg19_indel --latency-wait 100 -j 100 --cluster 'sbatch -t {params.runtime} --mem={resources.mem_mb} -c {threads} -p {params.partition}'

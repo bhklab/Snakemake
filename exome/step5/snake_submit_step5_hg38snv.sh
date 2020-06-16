8e4653f172d811f87457c01913ab06a7ca8bb5f7
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-06-16 16:24:52 -0400

    Update snake_submit_step5_hg38snv.sh

--- exome/step5/snake_submit_step5_hg38snv.sh
+++ exome/step5/snake_submit_step5_hg38snv.sh
@@ -8,4 +8,4 @@
 source /cluster/home/amammoli/.bashrc
 module load python3
 
-output_dir="/cluster/projects/bhklab/Data/TFRI_Exome" snakemake -s /cluster/projects/bhklab/Data/TFRI_Exome/step5/Snakefile_vcftomaf_hg38_snv --latency-wait 100 -j 100 --cluster 'sbatch -t {params.runtime} --mem={resources.mem_mb} -c {threads} -p {params.partition}'
+output_dir="/cluster/home/amammoli/exome" snakemake -s /path/to/gitclone/step5/Snakefile_vcftomaf_hg38_snv --latency-wait 100 -j 100 --cluster 'sbatch -t {params.runtime} --mem={resources.mem_mb} -c {threads} -p {params.partition}'

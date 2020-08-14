82f1d40ba3fb3b24bc76d08d661c8def7c513e5c
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-08-14 15:51:18 -0400

    Update snake_submit.sh

--- kallisto/snake_submit.sh
+++ kallisto/snake_submit.sh
@@ -8,4 +8,4 @@
 source /cluster/home/amammoli/.bashrc
 module load python3
 
-output_dir="/cluster/home/amammoli/out" transcriptome="/cluster/projects/bhklab/Data/RNAseqIndices/kallisto/gencodev33.kalliso.v461.idx" snakemake -s /cluster/home/amammoli/kallisto/Snakefile --latency-wait 100 -j 1 --cluster 'sbatch -t {params.runtime} --mem={resources.mem_mb} -c {threads}'
+output_dir="/cluster/home/amammoli/out" transcriptome="/cluster/projects/bhklab/references/RNAseqIndices/kallisto/gencodev33.kallisto.v461.idx" snakemake -s /cluster/home/amammoli/kallisto/Snakefile --latency-wait 100 -j 100 --cluster 'sbatch -t {params.runtime} --mem={resources.mem_mb} -c {threads}'

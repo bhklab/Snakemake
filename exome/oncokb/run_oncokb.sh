115ca0bedfa063b8bfd4dc2fbb87b2783d9b2b9b
Petr Smirnov <psmirnov2000@gmail.com>
2022-07-04 10:52:32 -0400

    remove key from file

--- exome/oncokb/run_oncokb.sh
+++ exome/oncokb/run_oncokb.sh
@@ -6,7 +6,7 @@ module load python3
 
 python /mnt/work1/users/bhklab/TFRI_Exome/oncokb-annotator/MafAnnotator.py -i /mnt/work1/users/bhklab/TFRI_Exome/MAF_hg38/indel/${samp}/0000.maf \
   -o /mnt/work1/users/bhklab/TFRI_Exome/hg38/indel/${samp}/0000_onco.maf \
-  -b ***REMOVED***
+  -b <INSERT KEY HERE>
 
 
 " >> /mnt/work1/users/bhklab/TFRI_Exome/jobs/${samp}_indel_38_0000.sh
